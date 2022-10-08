06954cd44f6729dd6e519010593c225e1c9725aa
TheJonaCode <mcjhona069@gmail.com>
2022-10-07 20:54:06 -0500

    adding spinner

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 const pokemonContainer = document.querySelector('.pokemon__container');
+const spinner = document.querySelector('#spinner');
 
 function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
         .then((res) => res.json())
         .then((data) => {
             createPokemon(data);
+            /*Spinner oculto*/
+            spinner.style.display = "none";
         });
 }
 
 function fetchPokemons(number) {
+    /*Spinner se muestra*/
+    spinner.style.display = "block";
     for (let i = 1; i <= number; i++) {
         fetchPokemon(i);
     }
 }
 
+function showPokemon() {
+    const cardPokemon = document.createElement('div');
+    cardPokemon.classList.add('pokemon__bio');
+    const infoPokemon = document.createElement('div');
+    infoPokemon.classList.add('pokemon__information');
+    cardPokemon.appendChild(infoPokemon);
+    pokemonContainer.appendChild(cardPokemon);
+};
+
 function createPokemon(pokemon) {
     const card = document.createElement('div');
     card.classList.add('pokemon__block');
+    card.addEventListener("click", showPokemon);
     //Contenedor imagen
     const spriteContainer = document.createElement('div');
     spriteContainer.classList.add('img__container');
